d2deb0290fb24a33997fc4684733c429b7b0c651
Vladimir Avinkin <vladimir.avinkin@sendcloud.com>
2024-07-15 18:29:41 +0200

    init dependency setup

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  version = "~> 5.57.0"
-}
-
 module "test" {
   source = "git::https://github.com/vladimir-avinkin/renovate-reproduce-terraform-bug.git//module?ref=v0.0.1"
 }
--- main2.tf
+++ main2.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.36.0"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = "~> 5.36.0"
+    }
+  }
+}
 module "test2" {
   source = "git::https://github.com/vladimir-avinkin/renovate-reproduce-terraform-bug.git//module?ref=v0.0.1"
 }
